a90434c6c98f63a6b4a6cac73560967c221639dd
lapis423 <63235621+lapis423@users.noreply.github.com>
2020-04-06 20:27:20 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	//1.변수선언
+	int num = 0;
+	
+	//2.메인화면 
+	printf("       CUSTOMER ACCOUNT BANKING MANAGEMENT SYSTEM\n");
+	printf("         ======= WELCOME TO THE MAIN MENU =======\n\n\n");
+	printf("  1.Create new account\n");
+	printf("  2.Update information of existing account\n");
+	printf("  3.For transactions\n");
+	printf("  4.Check the details of existing account\n");
+	printf("  5.Removing existiong account\n");
+	printf("  6.View customer's list\n");
+	printf("  7.Exit\n\n\n");
+	
+	
+	//3.입력 
+	printf("Enter your choice: ");
+	scanf("%d", &num);
+	
+	if (num == 1)
+	 char ID = a;
+	 printf("\n\nPlease Create Account.\n");
+	 printf("Account ID: ");
+	 scanf("%s", &ID);
+
+}
+
